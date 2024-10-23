41249612121b539b9b44a2431bf146da3474a4b1
house <housesellout@gmail.com>
2024-10-23 18:16:02 -0400

    … misc cleanup dead code

--- database/schemas/sowl_proc_schema.sql
+++ database/schemas/sowl_proc_schema.sql
@@ -37,13 +37,10 @@ BEGIN
 	            p_count as count_inp,
 	            p_desc as desc_inp
 	        FROM users u
-	        -- INNER JOIN referrals r
-	        --     ON u.id = r.fk_user_id
 	        INNER JOIN promotors p 
 	            ON u.id = p.fk_user_id
             INNER JOIN referrals r
                 ON r.tg_user_group_url = p.tg_user_group_url
-	        -- WHERE p.tg_user_group_url = @v_ref_link
 	        WHERE u.id = @v_user_id
 	        ORDER BY 
 	            r.is_active ASC,
@@ -116,141 +113,6 @@ BEGIN
 END 
 $$ DELIMITER ;
 
--- DELIMITER $$
--- DROP PROCEDURE IF EXISTS USER_JOIN_EVENT;
--- CREATE PROCEDURE `USER_JOIN_EVENT`(
---     IN p_tg_user_id VARCHAR(40), -- '581475171'
--- 	IN p_tg_user_at VARCHAR(1024), -- '@whatever'
--- 	IN p_tg_user_handle VARCHAR(1024), -- 'my handle'
---     IN p_tg_chat_id VARCHAR(40), -- '-1002003863532'
---     IN p_is_join BOOLEAN,
--- 	IN p_tg_user_group_url VARCHAR(1024) -- 't.me/<custom-link>' when join | '-1' when user leaves 
--- 	)
--- BEGIN
--- 	-- validate: tg_user_id exists (updates 'users.tg_user_at' if needed)
--- 	IF NOT valid_tg_user(p_tg_user_id, p_tg_user_at) THEN
--- 		-- add to users table
--- 		INSERT INTO users (
--- 				tg_user_id,
--- 				tg_user_at,
--- 				tg_user_handle
--- 			) VALUES (
--- 				p_tg_user_id,
--- 				p_tg_user_at,
--- 				p_tg_user_handle
--- 			);
-
--- 		-- get new user id
--- 		-- SELECT LAST_INSERT_ID() into @new_usr_id;
---     END IF;
-    
---     -- get id of this tg user
---     SELECT id FROM users WHERE tg_user_id = p_tg_user_id INTO @v_user_id;
-
---     -- fail: if chat_id|ref_link combo does not exist in promotors table
---     IF NOT valid_tg_user_group_url_prom(p_tg_chat_id, p_tg_user_group_url) THEN
---         SELECT 'failed' as `status`, 
---                 'invalid chat_id|ref_link for join' as info, 
---                 @v_user_id as user_id_ref,
---                 -- @v_valid_usr_ref as valid_usr_ref,
---                 p_tg_user_id as tg_user_id_inp,
---                 p_tg_user_at as tg_user_at_inp,
---                 p_tg_user_handle as tg_user_handle_inp,
---                 p_tg_chat_id as tg_chat_id_inp,
---                 p_is_join as is_join_inp,
---                 p_tg_user_group_url as tg_user_group_url_inp;
---     ELSE
-
---         SELECT id FROM referrals 
---             WHERE fk_user_id = @v_user_id 
---                 AND tg_chat_id = p_tg_chat_id
---                 AND tg_user_group_url = p_tg_user_group_url
---             INTO @v_ref_id;
---         IF @v_ref_id IS NULL THEN -- found no user_id|chat_id|ref_link combo
---             -- add to referrals table 
---             --  w/ promotors id bound to chat_id|invite_link combo in promotors table
---             SELECT id FROM promotors 
---                 WHERE tg_chat_id = p_tg_chat_id 
---                     AND tg_user_group_url = p_tg_user_group_url 
---                 INTO @v_fk_user_prom_id;
---             INSERT INTO referrals (
---                     fk_user_id,
---                     fk_user_prom_id,
---                     tg_chat_id,
---                     tg_user_group_url
---                 ) VALUES (
---                     @v_user_id,
---                     @v_fk_user_prom_id,
---                     p_tg_chat_id,
---                     p_tg_user_group_url
---                 );
---         END IF;
-
---         SELECT id FROM referrals 
---             WHERE fk_user_id = @v_user_id 
---                 AND tg_chat_id = p_tg_chat_id
---                 AND tg_user_group_url = p_tg_user_group_url
---             INTO @v_ref_id;
---         SELECT is_active FROM referrals where id = @v_ref_id INTO @v_is_active;
---         IF @v_is_active THEN
---             SELECT 'failed' as `status`,
---                     'tg_user_id is_active already' as info, 
---                     @v_user_id as user_id_ref,
---                     @v_valid_usr_ref as valid_usr_ref,
---                     p_tg_user_id as tg_user_id_inp,
---                     p_tg_user_at as tg_user_at_inp,
---                     p_tg_user_handle as tg_user_handle_inp,
---                     p_tg_chat_id as tg_chat_id_inp,
---                     p_is_join as is_join_inp,
---                     p_tg_user_group_url as tg_user_group_url_inp;
---         ELSE
---             -- set user is_active status for this ref_id: TRUE = currently in the group, FALSE = has left the group
---             UPDATE referrals SET dt_updated = NOW(), is_active = 1 WHERE id = @v_ref_id;
-
---             -- calc & set promotor's points (increment|decrement)
---             SELECT referral_points FROM promotors WHERE tg_chat_id = p_tg_chat_id INTO @v_OG_pts_prom;
---             SELECT id FROM promotors 
---                 WHERE tg_user_group_url = p_tg_user_group_url
---                     AND tg_chat_id = p_tg_chat_id
---                 INTO @v_usr_prom_id;
---             UPDATE promotors 
---                 SET dt_updated = NOW(), 
---                     referral_points = CASE 
---                         WHEN p_is_join = 1 THEN referral_points + 1
---                         WHEN p_is_join = 0 AND referral_points > 0 THEN referral_points - 1
---                         ELSE referral_points
---                     END
---                 WHERE id = @v_usr_prom_id;
-
---             -- return
---             SELECT u.id as u_id, u.tg_user_id, u.tg_user_at, u.tg_user_handle, 
---                     r.id as r_id, r.fk_user_id as fk_user_id_ref, r.fk_user_prom_id as fk_user_prom_id_ref, r.tg_user_group_url as tg_user_group_url_ref, r.is_active as new_is_active,
---                     p.id as p_id, p.fk_user_id as fk_user_id_prom, p.tg_user_group_url as tg_user_group_url_prom, p.referral_points as new_total_pts,
---                     'success' as `status`,
---                     'updated referral is_active status & promotor ref_pts' as info,
---                     @v_user_id as user_id_ref,
---                     @v_valid_usr_ref as valid_usr_ref,
---                     @v_fk_user_prom_id as fk_user_prom_id,
---                     @v_tg_user_group_url_ref as ref_tg_user_group_url,
---                     @v_ref_id as ref_id,
---                     @v_OG_pts_prom as OG_pts,
---                     p_tg_user_id as tg_user_id_inp,
---                     p_tg_user_at as tg_user_at_inp,
---                     p_tg_user_handle as tg_user_handle_inp,
---                     p_tg_chat_id as tg_chat_id_inp,
---                     p_is_join as is_join_inp,
---                     p_tg_user_group_url as tg_user_group_url_inp
---                 FROM referrals r
---                 INNER JOIN users u
---                     ON u.id = r.fk_user_id
---                 INNER JOIN promotors p
---                     ON p.tg_user_group_url = r.tg_user_group_url
---                 WHERE r.id = @v_ref_id;
---         END IF;
---     END IF;
--- END 
--- $$ DELIMITER ;
-
 -- # '/aux_referral_event'
 DELIMITER $$
 DROP PROCEDURE IF EXISTS EXE_REFERRAL_EVENT;
@@ -264,13 +126,6 @@ CREATE PROCEDURE `EXE_REFERRAL_EVENT`(
 	IN p_tg_user_group_url VARCHAR(1024) -- 't.me/<custom-link>' when join | '-1' when user leaves 
 	)
 BEGIN
-	-- -- fail: if tg_user_at is taken
-	-- IF valid_tg_user_at(p_tg_user_at) THEN
-	-- 	SELECT 'failed' as `status`, 
-	-- 			'user already exists; contact support' as info, 
-	-- 			p_tg_user_id as tg_user_id_inp,
-	-- 			p_tg_user_at as tg_user_at_inp;
-
 	-- validate: tg_user_id exists (updates 'users.tg_user_at' if needed)
 	IF NOT valid_tg_user(p_tg_user_id, p_tg_user_at) THEN
 		-- add to users table
@@ -402,16 +257,6 @@ BEGIN
                         ELSE referral_points
                     END
                 WHERE id = @v_usr_prom_id;
-            -- SELECT referral_points FROM promotors WHERE tg_user_group_url = p_tg_user_group_url INTO @v_pts;
-            -- IF p_is_join THEN -- inc points
-            --     -- SET @v_pts = @v_pts + 1;
-            --     -- update user points calc'd in promotors
-            --     UPDATE promotors SET referral_points = @v_pts + 1 WHERE tg_user_group_url = p_tg_user_group_url;
-            -- ELSEIF @v_pts > 0 THEN -- decr points only if above 0
-            --     -- SET @v_pts = @v_pts - 1;
-            --     -- update user points calc'd in promotors
-            --     UPDATE promotors SET referral_points = @v_pts -1 WHERE tg_user_group_url = p_tg_user_group_url;
-            -- END IF;
 
             -- return
             SELECT u.id as u_id, u.tg_user_id, u.tg_user_at, u.tg_user_handle, 
@@ -438,54 +283,6 @@ BEGIN
                 INNER JOIN promotors p
                     ON p.tg_user_group_url = r.tg_user_group_url
                 WHERE r.id = @v_ref_id;
-            -- SELECT COUNT(*)
-            --     FROM referrals r
-            --     INNER JOIN users u
-            --         ON u.id = r.fk_user_id
-            --     INNER JOIN promotors p
-            --         ON p.tg_user_group_url = r.tg_user_group_url
-            --     WHERE r.id = @v_ref_id
-            --     INTO @v_ret_cnt;
-            -- IF @v_ret_cnt = 0 THEN
-            --     SELECT 'failed' as `status`,
-            --             'proc exe returned 0 rows, return inp & vars' as info,
-            --             @v_user_id as user_id_ref,
-            --             @v_valid_usr_ref as valid_usr_ref,
-            --             @v_fk_user_prom_id as fk_user_prom_id,
-            --             @v_tg_user_group_url_ref as ref_tg_user_group_url,
-            --             @v_ref_id as ref_id,
-            --             @v_OG_pts_prom as OG_pts,
-            --             p_tg_user_id as tg_user_id_inp,
-            --             p_tg_user_at as tg_user_at_inp,
-            --             p_tg_user_handle as tg_user_handle_inp,
-            --             p_tg_chat_id as tg_chat_id_inp,
-            --             p_is_join as is_join_inp,
-            --             p_tg_user_group_url as tg_user_group_url_inp;
-            -- ELSE
-            --     SELECT u.id as u_id, u.tg_user_id, u.tg_user_at, u.tg_user_handle, 
-            --             r.id as r_id, r.fk_user_id as fk_user_id_ref, r.fk_user_prom_id as fk_user_prom_id_ref, r.tg_user_group_url as tg_user_group_url_ref, r.is_active as new_is_active,
-            --             p.id as p_id, p.fk_user_id as fk_user_id_prom, p.tg_user_group_url as tg_user_group_url_prom, p.referral_points as new_total_pts,
-            --             'success' as `status`,
-            --             'updated referral is_active status & promotor ref_pts' as info,
-            --             @v_user_id as user_id_ref,
-            --             @v_valid_usr_ref as valid_usr_ref,
-            --             @v_fk_user_prom_id as fk_user_prom_id,
-            --             @v_tg_user_group_url_ref as ref_tg_user_group_url,
-            --             @v_ref_id as ref_id,
-            --             @v_OG_pts_prom as OG_pts,
-            --             p_tg_user_id as tg_user_id_inp,
-            --             p_tg_user_at as tg_user_at_inp,
-            --             p_tg_user_handle as tg_user_handle_inp,
-            --             p_tg_chat_id as tg_chat_id_inp,
-            --             p_is_join as is_join_inp,
-            --             p_tg_user_group_url as tg_user_group_url_inp
-            --         FROM referrals r
-            --         INNER JOIN users u
-            --             ON u.id = r.fk_user_id
-            --         INNER JOIN promotors p
-            --             ON p.tg_user_group_url = r.tg_user_group_url
-            --         WHERE r.id = @v_ref_id;
-            -- END IF;
         END IF;
     END IF;
 END 
@@ -500,19 +297,8 @@ CREATE PROCEDURE `ADD_TG_SOWL_PROMOTOR`(
 	IN p_tg_user_handle VARCHAR(1024), -- 'my handle'
     IN p_tg_chat_id VARCHAR(40), -- '-1002003863532'
 	IN p_tg_user_group_url VARCHAR(1024) -- 't.me/<custom-link>'
-    -- IN p_wallet_address VARCHAR(255),
-    -- IN p_tw_conf_url VARCHAR(1024),
-	-- IN p_tw_conf_id VARCHAR(40),
-	-- IN p_tw_user_at VARCHAR(255)
 	)
 BEGIN
-	-- -- fail: if tg_user_at is taken
-	-- IF valid_tg_user_at(p_tg_user_at) THEN
-	-- 	SELECT 'failed' as `status`, 
-	-- 			'user already exists; contact support' as info, 
-	-- 			p_tg_user_id as tg_user_id_inp,
-	-- 			p_tg_user_at as tg_user_at_inp;
-
 	-- validate: tg_user_id exists taken (updates 'users.tg_user_at' if needed)
 	IF NOT valid_tg_user(p_tg_user_id, p_tg_user_at) THEN
 		-- add to users table
